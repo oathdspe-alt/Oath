925b3e5a35e622821b76ea3690817391d5470646
Andre <oathdspe@gmail.com>
2026-04-04 07:09:02 -0500

    slug working

--- schemaTypes/collection.js
+++ schemaTypes/collection.js
@@ -3,8 +3,24 @@ export default {
   title: 'Colecciones',
   type: 'document',
   fields: [
-    { name: 'title', title: 'Nombre de la Colección', type: 'string' },
-    { name: 'slug', title: 'URL', type: 'slug', options: { source: 'title' } },
-    { name: 'image', title: 'Imagen', type: 'image' },
+    {
+      name: 'title',
+      title: 'Nombre de la Colección',
+      type: 'string'
+    },
+    {
+      name: 'slug',
+      title: 'URL (Slug)',
+      type: 'slug',
+      options: {
+        source: 'title',
+        maxLength: 96,
+      }
+    },
+    {
+      name: 'image',
+      title: 'Imagen',
+      type: 'image'
+    }
   ]
 }
