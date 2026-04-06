8d00cdf3ff6bf27f419e7eb9a3885782d33b1383
Andre <oathdspe@gmail.com>
2026-04-05 22:42:23 -0500

    home solo colecciones

--- oath-shop/app/page.tsx
+++ oath-shop/app/page.tsx
@@ -41,30 +41,3 @@ export default async function Home() {
           </a>
         ))}
       </div>
-
-      {/* PRODUCTOS */}
-      <h2 className="text-2xl mb-6">Productos</h2>
-      <div className="grid grid-cols-1 md:grid-cols-3 gap-8">
-        {products.map((p: any) => (
-          <div key={p._id} className="border border-white/10 p-4">
-            <img
-              src={urlFor(p.image).url()}
-              className="w-full h-[300px] object-cover"
-            />
-            <h2 className="text-xl mt-4">{p.name}</h2>
-            <p className="text-gray-400">S/ {p.price}</p>
-            <p className="text-sm text-gray-500">{p.collectionName}</p>
-
-            <a
-              href={`https://wa.me/51993764834?text=Hola quiero ${p.name}`}
-              target="_blank"
-              className="block mt-4 bg-white text-black text-center py-2"
-            >
-              Comprar
-            </a>
-          </div>
-        ))}
-      </div>
-    </main>
-  );
-}
